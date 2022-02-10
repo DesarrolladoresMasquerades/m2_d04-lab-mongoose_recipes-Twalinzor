f2d07b161265ad258f785920dd2cb120d01fc6fe
Stan T <86654387+Twalinzor@users.noreply.github.com>
2022-02-10 17:49:01 +0100

    Done

---
+++ create.js
@@ -0,0 +1,3 @@
+const Recipe = require("./models/Recipe.model.js");
+
+require("./index.js");
--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
-
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const data = require("./data");
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
+  .connect(
+    "mongodb+srv://Stan:ironhack2022@cluster0.wgkvi.mongodb.net/RecipeDB?retryWrites=true&w=majority"
+  )
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((response) => {
+    console.log("RESPONSE ======>", response);
+    // Recipe.create();
+
+    Recipe.insertMany(data)
+      .then(() =>
+      Recipe.findOneAndUpdate(
+        { title: "Rigatoni alla Genovese" },
+        { $set: { duration: 100 } },
+        { new: true }
+      )
+        .then(() => {
+          Recipe.deleteOne({ title: "Carrot Cake" }).then(() => {
+            console.log("carrot");
+          });
+        })
+    );
+  })    
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+mongoose.connection.close();
+  //Recipe.deleteOne({title:"Carrot Cake"})
+    //.then(()=>{console.log("Carrot Cake Deleted")})
+  
